0397615ef1eb4b000dbaac54bfa310f93727d4ba
Tangzhengqi <tangzhengqi@sinobiocan.com>
2026-02-12 20:59:39 +0800

    测试手机版是否可行

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,10 @@ const header = document.querySelector("[data-header]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navLinks = document.querySelectorAll("[data-navbar-link]");
 
-navToggleBtn.addEventListener("click", function () {
-  this.classList.toggle("active");
-  header.classList.toggle("nav-active");
-});
+// navToggleBtn.addEventListener("click", function () {
+//   this.classList.toggle("active");
+//   header.classList.toggle("nav-active");
+// });
 
 for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click", function () {
